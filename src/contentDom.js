495ef49e94b8e903f5a98ef5cd4d0a6e041f68d3
JunWeiLi233 <mcpejunwei@gmail.com>
2026-05-25 10:48:58 -0400

    Read professor name instructor metadata

--- src/contentDom.js
+++ src/contentDom.js
@@ -95,6 +95,7 @@ const ALBERT_OBSERVER_OPTIONS = {
     "data-pnlfld",
     "data-pnlfldid",
     "data-pnlfldname",
+    "data-professor-name",
     "data-ps-column-id",
     "data-ps-column-name",
     "data-ps-columnid",
@@ -810,7 +811,7 @@ function associatedLabelText(element) {
 }
 
 function firstNameLikeAttribute(element) {
-  for (const attributeName of ["title", "aria-label", "data-value", "data-label", "data-option-label", "data-option-text", "data-item-label", "data-item-text", "data-person-label", "data-person-name", "data-person-text", "data-faculty-label", "data-faculty-name", "data-faculty-text", "data-title", "data-caption", "data-description", "data-text", "data-search", "data-full-name", "data-fullname", "data-full-text", "data-tooltip", "data-content", "data-original-title", "data-display", "data-display-name", "data-displayname", "data-name"]) {
+  for (const attributeName of ["title", "aria-label", "data-value", "data-label", "data-option-label", "data-option-text", "data-item-label", "data-item-text", "data-person-label", "data-person-name", "data-person-text", "data-faculty-label", "data-faculty-name", "data-faculty-text", "data-professor-name", "data-title", "data-caption", "data-description", "data-text", "data-search", "data-full-name", "data-fullname", "data-full-text", "data-tooltip", "data-content", "data-original-title", "data-display", "data-display-name", "data-displayname", "data-name"]) {
     const value = element.getAttribute(attributeName)?.trim();
     if (value && splitInstructorList(value).some(isLikelyInstructorName)) {
       return value;
