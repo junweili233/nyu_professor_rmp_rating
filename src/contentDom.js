00e66d999d66d1647153889da913133f839f2ebc
JunWeiLi233 <mcpejunwei@gmail.com>
2026-05-25 09:48:04 -0400

    Read focus class instructor options

--- src/contentDom.js
+++ src/contentDom.js
@@ -538,10 +538,10 @@ function hasSelectedOptionClass(element) {
   return Array.from(element.classList ?? [])
     .some((className) => {
       const normalizedClassName = className.toLowerCase();
-      return /^(?:is-|ps-|ui-|nyu-)?(?:selected|active|current|checked|on|pressed|highlighted|focused)$/.test(normalizedClassName)
-        || /^(?:is|ps|ui|nyu)[-_](?:selected|active|current|checked|on|pressed|highlighted|focused)[-_]option$/.test(normalizedClassName)
-        || /^(?:selected|active|current|checked|on|pressed|highlighted|focused)[-_]option$/.test(normalizedClassName)
-        || /^option[-_](?:selected|active|current|checked|on|pressed|highlighted|focused)$/.test(normalizedClassName);
+      return /^(?:is-|ps-|ui-|nyu-)?(?:selected|active|current|checked|on|pressed|highlighted|focused|focus)$/.test(normalizedClassName)
+        || /^(?:is|ps|ui|nyu)[-_](?:selected|active|current|checked|on|pressed|highlighted|focused|focus)[-_]option$/.test(normalizedClassName)
+        || /^(?:selected|active|current|checked|on|pressed|highlighted|focused|focus)[-_]option$/.test(normalizedClassName)
+        || /^option[-_](?:selected|active|current|checked|on|pressed|highlighted|focused|focus)$/.test(normalizedClassName);
     });
 }
 
